310051e8fc88db3077127bc17e974b79b190f36b
sanjay <sanjaysgs94@gmail.com>
2025-02-01 21:23:25 +0530

    Add union_list prgm

---
+++ basic_programming/list_exercises/union_two_arrays.py
@@ -0,0 +1,24 @@
+def union_two_arrays(a, b):
+    union_list = []
+
+    for i in range(len(a)):
+        if a[i] not in union_list:
+            union_list.append(a[i])
+
+    for j in range(len(b)):
+        if b[j] not in union_list:
+            union_list.append(b[j])
+
+    return len(union_list)
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+print(union_two_arrays(a, b)) # Output: 5
+
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+print(union_two_arrays(a, b))  # Output: 0
+
+a = [1, 2, 1, 1, 2]
+b = [2, 2, 1, 2, 1] 
+print(union_two_arrays(a, b)) # Output: 2
