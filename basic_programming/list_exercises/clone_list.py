2b39905e81ffdd9e3272572444a3804314ba9143
sanjay <sanjaysgs94@gmail.com>
2025-02-02 16:33:17 +0530

    Add clone_or_copy list prgm

---
+++ basic_programming/list_exercises/clone_list.py
@@ -0,0 +1,20 @@
+#Using copy()
+a = [1, 2, 3, 4, 5]
+
+# Copying the list
+b = a.copy()
+print(b)
+
+
+#Using List Slicing
+a = [1, 2, 3, 4, 5]
+b= a[:]
+print(b)
+
+
+#Using list()
+a = [1, 2, 3, 4, 5]
+
+# Copying the list using list()
+b = list(a)
+print(b)
