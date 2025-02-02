af0ddaca673ccb3c8ac9d2f0b8841f36e6c12dd9
sanjay <sanjaysgs94@gmail.com>
2025-02-02 16:06:52 +0530

    Add reverse list prgm

---
+++ basic_programming/list_exercises/reverse_list.py
@@ -0,0 +1,17 @@
+a = [10, 20, 30, 40, 50]
+
+a.reverse()
+print(a)
+
+# Output: [50, 40, 30, 20, 10]
+
+#Slicing Syntax:
+#list[start:stop:step]
+
+a = [1, 2, 3, 4, 5]
+
+# Create a new list that is a reversed list
+# of 'a' using slicing
+rev = a[::-1]
+
+print(rev)
