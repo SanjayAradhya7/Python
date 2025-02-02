fa1da5107dc613271cf65ad770c36562f93d16a6
sanjay <sanjaysgs94@gmail.com>
2025-02-02 13:49:22 +0530

    Updated output of union prgm

--- basic_programming/list_exercises/union_two_arrays.py
+++ basic_programming/list_exercises/union_two_arrays.py
@@ -2,14 +2,14 @@ def union_two_arrays(a, b):
     union_list = []
 
     for i in range(len(a)):
-        if a[i] != a[i+1]:
+        if a[i] not in union_list:
             union_list.append(a[i])
 
     for j in range(len(b)):
-        if b[j] == union_list:
+        if b[j] not in union_list:
             union_list.append(b[j])
 
-    return (union_list)
+    return len(union_list)
 
 a = [1, 2, 3, 4, 5]
 b = [1, 2, 3, 4, 5]
@@ -17,7 +17,7 @@ print(union_two_arrays(a, b)) # Output: 5
 
 a = [1, 2, 3, 4, 5]
 b = [6, 7, 8, 9, 10]
-print(union_two_arrays(a, b))  # Output: 0
+print(union_two_arrays(a, b))  # Output: 10
 
 a = [1, 2, 1, 1, 2]
 b = [2, 2, 1, 2, 1] 
