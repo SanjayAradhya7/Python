aee0557ac7e448043d47df307a4f58ebcef456a1
sanjay <sanjaysgs94@gmail.com>
2025-02-02 15:52:38 +0530

    Add element exists prgm

---
+++ basic_programming/list_exercises/element_exists.py
@@ -0,0 +1,14 @@
+a = [1, 2, 3, 4, 5]
+if 3 in a:
+    print("Element exists")
+else:
+    print("Element does not exist")
+
+
+a = [10, 20, 30, 40, 50]
+
+# Check if 30 exists in the list using count()
+if a.count(30) > 0:
+    print("Element exists in the list")
+else:
+    print("Element does not exist")
